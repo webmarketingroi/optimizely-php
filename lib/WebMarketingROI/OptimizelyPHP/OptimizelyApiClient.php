7b13611cd8b13b474215f6b1a08b0e391017780f
Oleg Krivtsov <zexspectrum@gmail.com>
2016-11-16 18:27:45 +0700

    Fixing unit tests

--- lib/WebMarketingROI/OptimizelyPHP/OptimizelyApiClient.php
+++ lib/WebMarketingROI/OptimizelyPHP/OptimizelyApiClient.php
@@ -231,6 +231,14 @@ class OptimizelyApiClient
         $headers[] = "Content-length:" . strlen($content);            
         
         // Set HTTP options.
+        if (!function_exists('curl_reset'))
+        {
+            function curl_reset(&$ch)
+            {
+                $ch = curl_init();
+            }
+        }
+        
         curl_reset($this->curlHandle);
         curl_setopt($this->curlHandle, CURLOPT_URL, $url);
         curl_setopt($this->curlHandle, CURLOPT_CUSTOMREQUEST, $method);        
